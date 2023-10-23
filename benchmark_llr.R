6986fe6c7b7f38bece43d0a01f7e0cf6647cc66c
Ekaterina <44580299+katyalex@users.noreply.github.com>
2023-10-23 12:33:35 -0400

    fix benchmark

--- benchmark_llr.R
+++ benchmark_llr.R
@@ -1,13 +1,6 @@
 setwd("~/Documents/GitHub/s610_lab3")
 source("llr_functions.R")
-source("test_llr.R")
-omega = 2
 # install.packages("bench")
-library(bench)
+library(microbenchmark)
 
-
-microbenchmark::microbenchmark(
-  llr(x, y, z, omega = 1),
-  make_weight_matrix(0.2,x,omega = 2),
-  make_predictor_matrix(rnorm(5)))
-
+microbenchmark(llr)
